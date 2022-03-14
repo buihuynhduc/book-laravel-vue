47036fede6f87e70c15110dce5995b39598907b0
buihuynhduc <huynhduc01693495928@gmail.com>
2022-03-14 10:41:02 +0700

    create

---
+++ resources/js/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+ <h1>
+     Hello {{duc.name}}
+ </h1>
+</template>
+
+<script>
+var data = {
+   duc: {
+       name: 'buihuynhduc',
+       id: '1'
+   }
+};
+export default {
+    name: "Hello",
+    data(){
+        return data;
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
